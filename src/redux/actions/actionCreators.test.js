a2dcfb59d9b947cf7fbf9acc7084e6b2f370d654
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-06 18:14:15 +0100

    Added test for actionCreator loadRobots

--- src/redux/actions/actionCreators.test.js
+++ src/redux/actions/actionCreators.test.js
@@ -1,7 +1,19 @@
-/*describe("Given a component actionCreator", () => {
+import { getRandomRobots } from "../../factories/robotFactory";
+import { loadRobotsAction } from "./actionCreators";
+import actionTypes from "./actionTypes";
+
+describe("Given a component actionCreator", () => {
   describe("When it receives a list of robots", () => {
-     test("Then it should return a load type action with the robot received", () => {
-       const robotsList = getRandomRobots();
-     });
+    test("Then it should return a load type action with the robot received", () => {
+      const robotsList = getRandomRobots();
+      const expectedAction = {
+        type: actionTypes.loadRobots,
+        robots: robotsList,
+      };
+
+      const actionResult = loadRobotsAction(robotsList);
+
+      expect(actionResult).toEqual(expectedAction);
+    });
   });
 });
