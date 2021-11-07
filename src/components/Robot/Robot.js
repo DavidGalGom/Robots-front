98ad5128d3806128f7d5c9467d662e1270e4bb54
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-07 12:44:40 +0100

    Fixed http of the images as src

--- src/components/Robot/Robot.js
+++ src/components/Robot/Robot.js
@@ -3,7 +3,9 @@ const Robot = ({ id, name, image, speed, resistance, creationDate }) => {
     <>
       <ul>
         <li>Robot Name: {name}</li>
-        <li>{image}</li>
+        <li>
+          <img src={image} alt="Robot" height="200" />
+        </li>
         <li>Robot Speed: {speed}</li>
         <li>Robot resistance: {resistance}</li>
         <li>Date of creation: {creationDate}</li>
