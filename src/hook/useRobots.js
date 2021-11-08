481902f5f143bb09ea1c90a6f7fbc8df0526af94
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-08 22:03:12 +0100

    Added loginUser,logoutUser and usedIsLogged at custom hook

--- src/hook/useRobots.js
+++ src/hook/useRobots.js
@@ -1,9 +1,15 @@
+import jwtDecode from "jwt-decode";
 import { useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import {
+  logoutUserAction,
+  userIsLoggedAction,
+} from "../redux/actions/actionCreators";
 import {
   loadRobotsThunk,
   createRobotThunk,
   deleteRobotThunk,
+  loginUserThunk,
 } from "../redux/thunks/thunks";
 
 const useRobots = () => {
@@ -26,11 +32,33 @@ const useRobots = () => {
     },
     [dispatch]
   );
+
+  const loginUser = (user) => {
+    dispatch(loginUserThunk(user));
+  };
+
+  const logoutUser = () => {
+    localStorage.removeItem("token");
+    dispatch(logoutUserAction());
+  };
+
+  const userIsLogged = useCallback(() => {
+    const userIsLogged = JSON.parse(localStorage.getItem("user"));
+
+    if (userIsLogged) {
+      const userData = jwtDecode(userIsLogged.token);
+      dispatch(userIsLoggedAction(userData));
+    }
+  }, [dispatch]);
+
   return {
     robots,
     loadRobots,
     createRobot,
     deleteRobot,
+    loginUser,
+    logoutUser,
+    userIsLogged,
   };
 };
 
