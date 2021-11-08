d5fb8df0199c26dd5ed4f25bd97e986cc090670a
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-08 20:09:59 +0100

    Added Routes for SPA

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import "./App.css";
-import FormCreate from "./components/FormCreate/FormCreate";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 import Header from "./components/Header/Header";
-import List from "./components/List/List";
+
+import RobotList from "./pages/RobotList/RobotList";
+import Login from "./pages/Login/Login";
+import Logout from "./pages/Logout/Logout";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
     <>
-      <h1>Robots: FIRST Full stack App</h1>
-      <Header />
-      <List />
-      <FormCreate />
+      <Router>
+        <h1>Robots: FIRST Full stack App</h1>
+        <Header />
+        <Switch>
+          <Route path="/Login" exact>
+            <Login />
+          </Route>
+          <Route path="/RobotList" exact>
+            <RobotList />
+          </Route>
+          <Route path="/Logout" exact>
+            <Logout />
+          </Route>
+          <Route path="/" exact>
+            <Redirect to="/RobotList" />
+          </Route>
+          <Route path="*">
+            <NotFoundPage />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
---
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,9 @@
+function NotFoundPage() {
+  return (
+    <>
+      <h2>Error, Page not Found</h2>
+    </>
+  );
+}
+
+export default NotFoundPage;
