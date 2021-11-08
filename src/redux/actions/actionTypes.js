0f50d3ff10f0d948f7f0eaa20a5815294fae904b
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-08 21:23:10 +0100

    Added action types for log in and out a user

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -2,5 +2,7 @@ const actionTypes = {
   loadRobots: "LOAD_ROBOTS",
   createRobot: "CREATE_ROBOT",
   deleteRobot: "DELETE_ROBOT",
+  loginUser: "LOGIN_USER",
+  logoutUser: "LOGOUT_USER",
 };
 export default actionTypes;
