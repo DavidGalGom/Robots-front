b4aaa34eafe7103081d60212d9f89fa101c71b5f
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-09 22:18:55 +0100

    Added conditional rendering for log in log out

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,28 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <ul className="header">
-      <li>
-        <Link to="/Login">Login</Link>
-      </li>
-      <li>
-        <Link to="/RobotList">List of Robots</Link>
-      </li>
-      <li>
-        <Link to="/Logout">Logout</Link>
-      </li>
-    </ul>
-  );
+  const isAuth = useSelector((store) => store.user.isAuthenticated);
+  if (isAuth === true) {
+    return (
+      <ul className="header">
+        <li>
+          <Link to="/RobotList">List of Robots</Link>
+        </li>
+        <li>
+          <Link to="/Logout">Logout</Link>
+        </li>
+      </ul>
+    );
+  } else {
+    return (
+      <ul className="header">
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
+      </ul>
+    );
+  }
 };
 
 export default Header;
